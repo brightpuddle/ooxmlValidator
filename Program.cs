2d4b4e4826c58f487657a3897362e9fd89e6ea3e
Nathan Hemingway <nhemingway@gmail.com>
2023-02-10 09:47:39 -0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Validation;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+static void ValidateWordDocument(string filepath)
+{
+    using (
+        WordprocessingDocument wordprocessingDocument = WordprocessingDocument.Open(filepath, true)
+    )
+    {
+        try
+        {
+            OpenXmlValidator validator = new OpenXmlValidator();
+            int count = 0;
+            foreach (ValidationErrorInfo error in validator.Validate(wordprocessingDocument))
+            {
+                count++;
+                Console.WriteLine("Error " + count);
+                Console.WriteLine("Description: " + error.Description);
+                Console.WriteLine("ErrorType: " + error.ErrorType);
+                Console.WriteLine("Node: " + error.Node);
+                Console.WriteLine("Path: " + error.Path.XPath);
+                Console.WriteLine("Part: " + error.Part.Uri);
+                Console.WriteLine("-------------------------------------------");
+            }
+
+            Console.WriteLine("count={0}", count);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        wordprocessingDocument.Close();
+    }
+}
+
+if (args.Length > 0)
+{
+    ValidateWordDocument(args[0]);
+}
